795ec283ad14524c92be3d3fa92eef502ab12a81
salehmu <ghd@keemail.me>
2022-03-19 02:23:47 +0200

    playground created

---
+++ csharp/playground/FoodApp/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, 
+                         IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
