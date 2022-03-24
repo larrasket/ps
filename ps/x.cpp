312f10abd01634fd7324d1c5bae0217e8934d870
salehmu <ghd@keemail.me>
2022-03-24 02:53:02 +0200

    playing

--- ps/x.cpp
+++ ps/x.cpp
@@ -1,27 +1,43 @@
 #include <algorithm>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-#include <time.h>
-using std::cout;
-using std::endl;
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
 
-void printArr(int arr[], int n) {
-  for (int i = 0; i < 10; i++)
-    cout << arr[i] << endl;
-}
-
-void yastShuffle(int arr[], int n) {
-  srand(time(NULL));
-  for (int i = n - 1; i > 0; i--) {
-    int j = rand() % (i + 1);
-    std::swap(arr[i], arr[j]);
+int searching(vector<int> arr, int x) {
+  int l = 0, r = arr.size(), mid;
+  while (l <= r) {
+    mid = l + (r - l) / 2;
+    if (arr[mid] == x)
+      return mid;
+    if (arr[mid] < x)
+      l = mid + 1;
+    else
+      r = mid - 1;
   }
+  if (arr[mid] > x)
+    return mid - 1;
+  return mid;
 }
 
 int main() {
-  int arr[] = {10, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  yastShuffle(arr, n);
-  printArr(arr, n);
-  return 0;
+  std::vector<int> mv{1, 10, 19, 20, 23};
+  cout << mv[searching(mv, 20)];
 }
