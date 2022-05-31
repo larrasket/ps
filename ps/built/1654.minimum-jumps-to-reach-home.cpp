1ee5eaa5d46227135b8a31bff1dbad62bccc00c5
salehmu <74098495+salehmu@users.noreply.github.com>
2022-05-31 04:49:55 +0200

    solved 1654 lc

--- ps/built/1654.minimum-jumps-to-reach-home.cpp
+++ ps/built/1654.minimum-jumps-to-reach-home.cpp
@@ -1,52 +1,41 @@
-/*
- * Problem: https://leetcode.com/problems/minimum-jumps-to-reach-home/
- * Tags: Dynamic Programming
- */
-
-#include <algorithm>
-#include <array>
-#include <climits>
-#include <cmath>
-#include <deque>
-#include <iomanip>
-#include <iostream>
-#include <iterator>
-#include <list>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <tuple>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#define begend(v) ((v).begin()), ((v).end())
-#define sz(v) ((int)((v).size()))
-using namespace std;
-int r, l;
-int Reach(int &Forward, int &Backward, int Position, int &Target,
-          vector<int> &Forbidden, int Steps) {
-  if (Position < Target) {
-    if (find(begend(Forbidden), Position + Forward) == Forbidden.end())
-      return Reach(Forward, Backward, Position + Forward, Target, Forbidden,
-                   Steps + 1);
-  } else if (Position > Target) {
-    if (find(begend(Forbidden), Position - Backward) == Forbidden.end() &&
-        Position - Backward >= 0)
-      return Reach(Forward, Backward, Position - Backward, Target, Forbidden,
-                   Steps + 1);
-  }
-  return Target == Position ? Steps : -1;
-}
-int main() {
-  vector<int> f{162, 118, 178, 152, 167, 100, 40,  74,  199, 186, 26,
-                73,  200, 127, 30,  124, 193, 84,  184, 36,  103, 149,
-                153, 9,   54,  154, 133, 95,  45,  198, 79,  157, 64,
-                122, 59,  71,  48,  177, 82,  35,  14,  176, 16,  108,
-                111, 6,   168, 31,  134, 164, 136, 72,  98};
-
-  int a = 29, b = 98, x = 80;
-  cout << Reach(a, b, 0, x, f, 0);
-}
+class Solution {
+public:
+	int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+		unordered_set<int> forbi(forbidden.begin(), forbidden.end());
+		vector<vector<int>> visited(2,vector<int>(7000));
+		// visited array has two states:
+		// visited[0][i] means the ith cell was reached from its left hand side jumping forward,
+		// visited[1][i] means the ith cell was reached from its right hand side jumping backward.
+		queue<pair<int,bool>> qu;
+		// true means your last jump was backward jumping, you cannot jump backward in current step.
+		// false, otherwise.
+		qu.push({0,false});
+		visited[0][0] = 1;
+		visited[1][0] = 1;
+		int ans = 0;
+		while(!qu.empty()) {
+			int len = qu.size();
+			while(len > 0) {
+				len--;
+				int cur = qu.front().first;
+				bool flag = qu.front().second;
+				if(cur == x) {
+					return ans;
+				}
+				qu.pop();
+				int forward = cur + a;
+				int backward = cur - b;
+				if(forward < 7000 && visited[0][forward] == 0 && !forbi.count(forward)) {
+					qu.push({forward,false});
+					visited[0][forward] = 1;
+				}
+				if(backward >=0 && visited[1][backward] == 0 && !forbi.count(backward) && !flag) {
+					qu.push({backward,true});
+					visited[1][backward] = 1;
+				}
+			}
+			ans++;
+		}
+		return -1;
+	}
+};
