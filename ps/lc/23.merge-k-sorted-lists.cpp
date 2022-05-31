fdcb8970b704d738a1b515bdc993d6c6eb65b46c
salehmu <74098495+salehmu@users.noreply.github.com>
2022-05-31 04:25:40 +0200

    23 lc not solved

---
+++ ps/lc/23.merge-k-sorted-lists.cpp
@@ -0,0 +1,48 @@
+/*
+ * Problem: https://leetcode.com/problems/merge-k-sorted-lists/
+ * Tags: Linked List
+ */
+
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *mergeKLists(vector<ListNode *> &lists) {
+    int i = 0;
+    auto mt = 2;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
