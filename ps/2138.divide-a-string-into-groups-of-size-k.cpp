c6214bc2a729f3e63653a68b90041940e6f3aa35
salehmu <74098495+salehmu@users.noreply.github.com>
2022-06-18 07:38:35 +0200

    solved lc 2138

---
+++ ps/2138.divide-a-string-into-groups-of-size-k.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem: divide-a-string-into-groups-of-size-k
+ * Tags: Strings
+ */
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
+
+class Solution {
+public:
+  vector<string> divideString(string s, int k, char fill) {}
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+}
