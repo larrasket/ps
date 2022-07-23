5055002b58524bbe5ac8c01499c37c5626c8bfeb
salehmu <ghd@keemail.me>
2022-07-23 08:56:52 +0200

    solbed Sum of Digists Digital Roat

---
+++ ps/cw/SumofDigitsDigitalRoot.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// func DigitalRoot(n int) int {
+// }
+
+func slv(n int, ans *int) int {
+	if n <= 0 {
+		return 0
+	}
+	return slv(n/10, ans)
+}
+
+func main() {
+
+	var i, ans int = 12, 0
+	fmt.Print(slv(i, &ans))
+
+}
