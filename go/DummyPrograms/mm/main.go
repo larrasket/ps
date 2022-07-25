0a29b8e2165aaef50630053695b044410e46df51
salehmu <ghd@keemail.me>
2022-07-25 10:49:03 +0200

    dummy

--- go/DummyPrograms/mm/main.go
+++ go/DummyPrograms/mm/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"time"
 )
 
+func PrintTime(label string, t *time.Time) {
+	layout := "Month: Jan Year: 2006"
+	fmt.Println(label, t.Format(layout))
+}
 func main() {
+	current := time.Now()
+	specific := time.Date(1995, time.June, 9, 0, 0, 0, 0, time.Local)
+	unix := time.Unix(1433228090, 0)
+	PrintTime("Current", &current)
+	PrintTime("Specific", &specific)
+	PrintTime("UNIX", &unix)
+
 }
