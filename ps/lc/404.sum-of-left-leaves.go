aead4e9d58450bf954513632dc8a7267828ea4ce
salehmu <ghd@keemail.me>
2022-07-28 11:27:37 +0200

    solved lc 404

---
+++ ps/lc/404.sum-of-left-leaves.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	return solve(root, false)
+}
+
+func solve(root *TreeNode, left bool) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil && left {
+		return root.Val
+	}
+	return solve(root.Left, true) + solve(root.Right, false)
+}
