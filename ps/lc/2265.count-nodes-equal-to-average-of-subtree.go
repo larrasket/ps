42253e2a7385d6cf0d27fbf3465426b4f2e8c2c7
salehmu <ghd@keemail.me>
2022-07-28 11:27:42 +0200

    solved lc 2265

---
+++ ps/lc/2265.count-nodes-equal-to-average-of-subtree.go
@@ -0,0 +1,30 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var ans int
+
+func solve(root *TreeNode) (sum, cnt int) {
+	if root == nil {
+		return
+	}
+	lsum, lcnt := solve(root.Left)
+	rsum, rcnt := solve(root.Right)
+	sum = lsum + rsum + root.Val
+	cnt = lcnt + rcnt + 1
+	if root.Val == sum/cnt {
+		ans++
+	}
+	return
+
+}
+
+func averageOfSubtree(root *TreeNode) int {
+	ans = 0
+	solve(root)
+	return ans
+}
