61f78f60533ca361df844a411f9300301053d60d
salehmu <ghd@keemail.me>
2022-10-15 14:49:08 +0200

    Solve lc 327

---
+++ ps/lc/0327_count-of-range-sum.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
+typedef pair<int, int> pi;
+priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+class Solution {
+public:
+  int countRangeSum(vector<int> &nums, int lower, int upper) {
+    int l(0), n(nums.size()), sum(0), r(0);
+    while (l < r && r < n) {
+      sum += nums[l];
+    }
+  }
+};
+r
