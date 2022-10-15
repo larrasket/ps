8f01809017feecdd501bf871ceda94f1d63d9e15
salehmu <ghd@keemail.me>
2022-10-15 14:53:53 +0200

    Solved lc 242

---
+++ ps/lc/0242_valid-anagram.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
+typedef pair<int, int> pi;
+priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+class Solution {
+public:
+  bool isAnagram(string s, string t) {
+    sort(begend(s)), sort(begend(t));
+    if (s == t)
+      return true;
+    return false;
+  }
+};
