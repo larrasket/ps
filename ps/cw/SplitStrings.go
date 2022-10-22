84a9c32af73c1f87a80fe2ab913c0aaa041d91fd
salehmu <ghd@keemail.me>
2022-10-22 18:51:02 +0200

    Solve cw SplitStrings

---
+++ ps/cw/SplitStrings.go
@@ -0,0 +1,12 @@
+package kata
+
+func Solution(str string) []string {
+	var res []string
+	if len(str)%2 != 0 {
+		str += "_"
+	}
+	for i := 0; i < len(str); i += 2 {
+		res = append(res, str[i:i+2])
+	}
+	return res
+}
