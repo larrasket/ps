230ea0446469a8633cb6f8e3b13b2159e3daafeb
salehmu <ghd@keemail.me>
2022-11-10 11:08:14 +0200

    Fail lc 416

---
+++ ps/built/0416_partition-equal-subset-sum.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
+typedef pair<int, int> pi;
+priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+class Solution {
+public:
+  bool canPartition(vector<int> &nums) {
+    sort(begend(nums));
+    int n = nums.size(), sum(0);
+    for (int i = 0; i < n; i++)
+      sum += nums[i];
+    int sum2(0);
+    for (int i = 0; i < n; i++) {
+      sum2 += nums[i];
+      sum -= nums[i];
+      if (sum2 == sum)
+        return 1;
+    }
+
+    return false;
+  }
+};
