a2fe313e0d967389b7b9a38397dd9621237ee71e
salehmu <ghd@keemail.me>
2022-11-11 16:53:01 +0200

    Solve lc 226

---
+++ ps/lc/0226_invert-binary-tree.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define begend(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)((v).size()))
+using namespace std;
+typedef pair<int, int> pi;
+priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+// struct TreeNode {
+//   int val;
+//   TreeNode *left;
+//   TreeNode *right;
+//   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//   TreeNode(int x, TreeNode *left, TreeNode *right)
+//       : val(x), left(left), right(right) {}
+// };
+class Solution {
+
+public:
+  TreeNode *invertTree(TreeNode *root) {
+    if (!root)
+      return root;
+    auto right = invertTree(root->right);
+    auto left = invertTree(root->left);
+    root->left = right;
+    root->right = left;
+    return root;
+  }
+};
