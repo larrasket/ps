817633e863757534fd39b06edec4e95ff10042bd
salehmu <ghd@keemail.me>
2023-05-01 21:10:47 +0300

    Solve lc 1662

---
+++ lc/1662_check-if-two-string-arrays-are-equivalent.go
@@ -0,0 +1,13 @@
+package lc
+
+func arrayStringsAreEqual(word1 []string, word2 []string) bool {
+	var s1, s2 string
+	for _, v := range word2 {
+		s2 += v
+	}
+
+	for _, v := range word1 {
+		s1 += v
+	}
+	return s1 == s2
+}
