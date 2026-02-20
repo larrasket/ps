c5d60e8591ea8129881c38696ead49dfe231b00c
Salih Muhammed <root@lr0.org>
2026-02-20 21:08:59 +0200

    Solve 0100

---
+++ lc/0100.same-tree.go
@@ -0,0 +1,15 @@
+package lc
+
+import (
+	"fmt"
+)
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
